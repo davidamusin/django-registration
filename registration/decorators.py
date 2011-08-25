6c388f50ee58a3b320fc12a81566f0403f8a5899
David Amusin <david@eatmetrics.com>
2011-08-25 16:38:00 -0700

    Use 401 rather than 200 for login required.

--- registration/decorators.py
+++ registration/decorators.py
@@ -1,14 +1,22 @@
 
 import simplejson as json
 
+from django.contrib.sites.models import Site
 from django.http import HttpResponse
+
+class HttpResponseNotAuthorized(HttpResponse):
+    status_code = 401
+
+    def __init__(self, redirect_to):
+        HttpResponse.__init__(self)
+        self['WWW-Authenticate'] = 'Cookie realm="%s"' % Site.objects.get_current().name
 
 def json_login_required(view_func):
     def wrap(instance, request, *args, **kwargs):
         if request.user.is_authenticated():
             return view_func(instance, request, *args, **kwargs)
         response_data = json.dumps({ 'not_authenticated': True })
-        return HttpResponse(response_data, mimetype='application/json')
+        return HttpResponseNotAuthorized(response_data, mimetype='application/json')
     wrap.__doc__ = view_func.__doc__
     wrap.__dict__ = view_func.__dict__
     return wrap
