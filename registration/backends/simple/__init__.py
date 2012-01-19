f6b11269fc13a2e064850c9ecbc2ca7839c01613
David Amusin <david@eatmetrics.com>
2012-01-18 23:56:17 -0800

    security check for redirection to different host

--- registration/backends/simple/__init__.py
+++ registration/backends/simple/__init__.py
@@ -1,3 +1,5 @@
+import urlparse
+
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth import authenticate
@@ -59,7 +61,17 @@ class SimpleBackend(object):
         After registration, redirect to the user's account page.
         
         """
-        return (request.REQUEST.get(REDIRECT_FIELD_NAME, user.get_absolute_url()), (), {})
+
+        redirect_to = user.get_absolute_url()
+
+        if REDIRECT_FIELD_NAME in request.REQUEST:
+            redirect_to = request.REQUEST.get(REDIRECT_FIELD_NAME)
+            # Don't allow redirection to a different host.
+            netloc = urlparse.urlparse(redirect_to)[1]
+            if netloc and netloc != request.get_host():
+                redirect_to = settings.LOGIN_REDIRECT_URL
+
+        return (redirect_to, (), {})
 
     def post_activation_redirect(self, request, user):
         raise NotImplementedError
